12be33ec772552f115d5f6d2377c0db177382f81
Shiquan Sun <me@imsun.net>
2016-12-18 19:19:40 +0800

    check & create GitHub hooks

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const http = require('http')
 const path = require('path')
+const request = require('request')
 const shell = require('shelljs')
 const pm2 = require('pm2')
 const proxy = require('http-proxy').createProxyServer({})
 const config = require('./config')
 
 config.root = config.root || ''
+const hooksRe = new RegExp(`^(.*?//)*${config.host}/webhook`, 'i')
+const GITHUB_API_HOST = 'http://api.github.com'
+const headers = {
+	'User-Agent': 'easy-pm',
+	'Accept': 'application/vnd.github.v3+json'
+}
+if (config.token) {
+	headers.Authorization = `token ${config.token}`
+}
 
 const routes = {}
 
@@ -29,6 +39,40 @@ const apps = config.apps.map(app => {
 		}
 	}
 
+	if (app.repository) {
+		const [, owner, repo] = app.repository.match(/.*?github\.com.(.+?)\/(.+?)\.git/)
+
+		if (owner && repo) {
+			request({
+				headers,
+				url: `${GITHUB_API_HOST}/repos/${owner}/${repo}/hooks`
+			}, (err, res, body) => {
+				try {
+					const hooks = JSON.parse(body)
+					const haveHooks = hooks.reduce((prev, current) => {
+						return prev || hooksRe.test(current.config.url)
+					}, false)
+					if (!haveHooks) {
+						request({
+							headers,
+							method: 'POST',
+							url: `${GITHUB_API_HOST}/repos/${owner}/${repo}/hooks`,
+							json: {
+								name: 'web',
+								active: true,
+								events: ['push'],
+								config: {
+									url: `${config.host}/webhook`,
+									content_type: 'json'
+								}
+							}
+						})
+					}
+				} catch (e) {}
+			})
+		}
+	}
+
 	return {
 		name: app.path,
 		cwd: appPath,
@@ -45,9 +89,10 @@ http.createServer((req, res) => {
 	const host = req.headers.host.split(':')[0]
 	if (routes[host]) {
 		proxy.web(req, res, {
-			target: `http://localhost:${routes[host]}`
+			target: `http://127.0.0.1:${routes[host]}`
 		})
 	} else {
+		console.log(req.headers)
 		res.end()
 	}
 }).listen(process.env.PORT || 3000)
