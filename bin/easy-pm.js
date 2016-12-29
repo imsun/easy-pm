faf64d45b8dcdb52124334156070318810059668
Shiquan Sun <me@imsun.net>
2016-12-29 08:44:53 +0800

    fix cli

--- bin/easy-pm.js
+++ bin/easy-pm.js
@@ -3,15 +3,18 @@ const program = require('commander')
 const info = require('../package.json')
 const easyPm = require('../')
 
-program
-	.version(info.version)
-	.command('start <file>')
+program.version(info.version)
+
+program.command('start <file>')
+	.description('start service with config file')
 	.action(file => easyPm.start(file))
 
-program.command('list')
-	.action(() => easyPm.list())
-
 program.command('stop <file>')
+	.description('stop service with config file')
 	.action(file => easyPm.stop(file))
+
+program.command('list')
+	.description('list running applications')
+	.action(() => easyPm.list())
 
 program.parse(process.argv)
